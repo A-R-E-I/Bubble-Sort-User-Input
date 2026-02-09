472be6b34a8a54eaf53b2ecabe008e86f2ba1a97
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2026-02-09 07:56:40 -0500

    Add files via upload

---
+++ BubbleSortActivity.java
@@ -0,0 +1,14 @@
+package javaPack;
+
+public class BubbleSortActivity {
+
+	public static void main(String[] args) 
+	{
+		Sort arrange = new Sort();
+		System.out.println("This program will print out each "
+				+ "numnber inputed in order\n"
+				+ "Enter a number: ");
+		arrange.organize();
+	}
+
+}
---
+++ Sort.java
@@ -0,0 +1,27 @@
+package javaPack;
+
+public class Sort 
+{
+	public void organize()
+	{
+		int[] arr = {25, 10, 7, 32, 5};
+		
+		for(int i = 0; i < arr.length - 1; i++)
+		{
+			for(int j = 0; j < arr.length - i - 1; j++)
+			{
+				if(arr[j] > arr[j + 1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		
+		for(int n=0; n<arr.length; n++)
+		{
+			System.out.print(arr[n] + " ");
+		}
+	}
+}
